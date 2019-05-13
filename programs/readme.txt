239799de9e4c8fc9af8098800a46c3cb3d596c55
huge200890 <https://huge200890@github.com>
2019-05-13 09:13:37 -0500

    Update programs

---
+++ programs/readme.txt
@@ -0,0 +1,5 @@
+Download blast 
+
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/legacy.NOTSUPPORTED/2.2.26/blast-2.2.26-x64-linux.tar.gz
+tar -zxvf blast-2.2.26-x64-linux.tar.gz
+
--- scripts/exe_swiss.pl
+++ scripts/exe_swiss.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl -w
-
+$GLOBAL_PATH;
+BEGIN { 
+$GLOBAL_PATH='/storage/htc/bdm/jh7x3/DNSS2_github/DNSS2/';
+}
 $numArgs = @ARGV;
 if($numArgs != 3)
 {   
@@ -27,12 +30,12 @@ foreach $line (@array)
 	}
 	else
 	{
-		open(TEM, ">>$in/temp.fasta") || die("Couldn't open file temp.fasta\n"); 
+		open(TEM, ">$in/temp.fasta") || die("Couldn't open file temp.fasta\n"); 
 		print TEM $name."\n".$line.$name."\n".$line;
 		close TEM;
 
 		#`perl ./blast_search_swiss_prot.pl $in/temp.fasta ./blast-2.2.17/bin/blastpgp $in $out ../swiss_prot_2010/uniprot_sprot.fasta ../swiss_prot_2010/uniprot_sprot.dat $in/swiss_prot_euk_1.LOG ../swiss_prot_2010/uniprot_sprot.fasta`;
-		`perl ./blast_search_swiss_prot.pl $in/temp.fasta ./blast-2.2.17/bin/blastpgp $in $out ../swiss_prot_2016/uniprot_sprot.fasta ../swiss_prot_2016/uniprot_sprot.dat $in/swiss_prot_euk_1.LOG ../swiss_prot_2016/uniprot_sprot.fasta`;
+		`perl $GLOBAL_PATH/scripts/blast_search_swiss_prot.pl $in/temp.fasta $GLOBAL_PATH/programs/blast-2.2.17/bin/blastpgp $in $out ../swiss_prot_2016/uniprot_sprot.fasta ../swiss_prot_2016/uniprot_sprot.dat $in/swiss_prot_euk_1.LOG ../swiss_prot_2016/uniprot_sprot.fasta`;
 
 		`rm $in/temp.fasta`;
 	}
